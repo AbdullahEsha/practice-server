e02d1d0ac261d5209c27d3a4ba5be7446df21cf0
AbdullahIsha <shahariaresha@gmail.com>
2023-07-20 02:50:31 +0600

    deploy and push

---
+++ routes/bookRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const {
+  getAllBooks,
+  getSingleBook,
+  ceateNewBook,
+  updateBook,
+  deleteBook,
+  getAllBooksDesc,
+} = require('../controllers/bookController')
+const bookRouter = express.Router()
+
+bookRouter.get('/', getAllBooks)
+bookRouter.post('/', ceateNewBook)
+bookRouter.get('/dsc', getAllBooksDesc)
+bookRouter
+  .get('/:id', getSingleBook)
+  .patch('/:id', updateBook)
+  .delete('/:id', deleteBook)
+
+module.exports = bookRouter
---
+++ services/findItemById.js
@@ -0,0 +1,18 @@
+const { successResponse, errorResponse } = require('../helpers/response')
+
+const FindItemById = async (id, res, Model, text) => {
+  try {
+    const data = await Model.findById(id)
+    if (data) {
+      successResponse(
+        res,
+        { statusCode: 200, message: `${text} found successfully` },
+        data,
+      )
+    }
+  } catch (error) {
+    errorResponse(res, 404, `${text} not found! try again..`)
+  }
+}
+
+module.exports = { FindItemById }
